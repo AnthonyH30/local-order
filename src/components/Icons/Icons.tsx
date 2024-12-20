c40ffe7a9b4b0153e9631d794c168abb39f61995
anthonyhfranca <aharisson@uolinc.com>
2024-12-19 21:34:26 -0300

    Creating SideBar and starting the Aside Section

---
+++ src/components/Icons/Icons.tsx
@@ -0,0 +1,6 @@
+export  { HomeIcon } from "./icons/HomeIcon";
+export { HistoryIcon } from "./icons/HistoryIcon";
+export { OrangeIcon } from "./icons/OrangeIcon";
+export { SettingsIcon } from "./icons/SettingsIcon";
+export { LogOutIcon } from "./icons/LogOutIcon";
+export { SearchIcon} from "./icons/SearchIcon";
---
+++ src/components/index.ts
@@ -0,0 +1,3 @@
+export { Navbar } from './Navbar/Navbar';
+export { HomeIcon, HistoryIcon, OrangeIcon, SettingsIcon, LogOutIcon, SearchIcon } from './Icons/Icons';
+export { OrderList } from "./OrderList/OrderList";
